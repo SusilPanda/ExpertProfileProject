78c9d8fbf640661e18b90e1e5283e2b7ed59bee5
Abhishek Singh <Abhishek Singh>
2016-07-25 21:36:46 +0530

    aimation dashboard

--- js/common.js
+++ js/common.js
@@ -26,3 +26,14 @@ $('[data-toggle="offcanvas"]').click(function() {
     $('#wrapper').toggleClass('toggled');
   });
 });
+
+$(window).scroll(function() {
+  $(".slideanim").each(function(){
+    var pos = $(this).offset().top;
+
+    var winTop = $(window).scrollTop();
+    if (pos < winTop + 600) {
+      $(this).addClass("slide");
+    }
+  });
+});
